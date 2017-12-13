f92bd72fce620368b5e09e8e6b877391b62bf0ec
Dio Brando <7iscool77@gmail.com>
2017-12-13 15:08:09 +0100

    made it better

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const fuckoff = document.getElementById('butt');
-let square = document.querySelector('.square')
-
+let squaRed = document.querySelector('.square__red');
+let squareMagenta = document.querySelector('.square__magenta');
+let squareBlue = document.querySelector('.square__blue');
 
 const extend = () => {
-  square.classList.toggle('square__extend');
+  squaRed.classList.toggle('square__extend');
+  squareBlue.classList.toggle('square__extend');
+  squareMagenta.classList.toggle('square__extend');
 }
 
 fuckoff.addEventListener('click', extend);
